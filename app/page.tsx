5f4d0109d8f29534e7e5844f02e186cc3304abcf
Kevin Yang-Li <yanglikevin@gmail.com>
2024-12-24 16:38:03 -0500

    finished the hero section of the page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
-        <h1>hello</h1>
       </div>
     </main>
   );
